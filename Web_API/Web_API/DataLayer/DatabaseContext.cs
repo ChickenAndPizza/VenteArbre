f70c9de58ea85dc80fa1ca7e1cec07cd7142f6f3
B438P-01\admin <jeepsboucher@gmail.com>
2019-03-14 09:13:13 -0400

    Ajout du projet API

---
+++ Web_API/Web_API/DataLayer/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_API.DataLayer.Mapping;
+
+namespace Web_API.DataLayer
+{
+    public class DatabaseContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseAutoSnakeCaseMapping();
+            this.UseAutoDetectedMappings(modelBuilder);
+        }
+    }
+}
