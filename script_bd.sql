cdd97e869c2c67cb36535a5381c0e3df646089ca
Alice Lessard <alessard97@hotmail.com>
2019-03-28 09:00:07 -0400

    script bd

--- script_bd.sql
+++ script_bd.sql
@@ -17,8 +17,9 @@ CREATE TABLE customer(
         last_name    Varchar (30) NOT NULL ,
         email        Varchar (80) NOT NULL ,
         phone_number Varchar (10) NOT NULL ,
-	password     Varchar (30) NOT NULL ,
-        is_admin     Bool NOT NULL
+	    password     Varchar (30) NOT NULL ,
+        is_admin     Bool NOT NULL,
+        is_active   Bool NOT NULL
 	,CONSTRAINT customer_PK PRIMARY KEY (id)
 )ENGINE=InnoDB;
 
@@ -31,7 +32,8 @@ CREATE TABLE customer_order(
         id               Varchar (36) NOT NULL ,
         transaction_date Date NOT NULL ,
         state            TinyINT NOT NULL ,
-        id_customer      Varchar (36) NOT NULL
+        id_customer      Varchar (36) NOT NULL,
+        is_active        Bool NOT NULL
 	,CONSTRAINT customer_order_PK PRIMARY KEY (id)
 
 	,CONSTRAINT customer_order_customer_FK FOREIGN KEY (id_customer) REFERENCES customer(id)
@@ -46,7 +48,8 @@ CREATE TABLE supplier(
         id           Varchar (36) NOT NULL ,
         name         Varchar (100) NOT NULL ,
         phone_number Varchar (10) NOT NULL ,
-        email        Varchar (10) NOT NULL
+        email        Varchar (10) NOT NULL,
+        is_active    Bool NOT NULL
 	,CONSTRAINT supplier_PK PRIMARY KEY (id)
 )ENGINE=InnoDB;
 
@@ -59,7 +62,8 @@ CREATE TABLE supplier_order(
         id               Varchar (36) NOT NULL ,
         transaction_date Date NOT NULL ,
         id_customer      Varchar (36) NOT NULL ,
-        id_supplier      Varchar (36) NOT NULL
+        id_supplier      Varchar (36) NOT NULL,
+        is_active        Bool NOT NULL
 	,CONSTRAINT supplier_order_PK PRIMARY KEY (id)
 
 	,CONSTRAINT supplier_order_customer_FK FOREIGN KEY (id_customer) REFERENCES customer(id)
@@ -86,10 +90,10 @@ CREATE TABLE tree_category(
 
 
 CREATE TABLE tree_sub_category(
-        id          Varchar (36) NOT NULL ,
-        description Varchar (254) NOT NULL ,
+        id               Varchar (36) NOT NULL ,
+        description      Varchar (254) NOT NULL ,
 		id_tree_category VARCHAR(36) NOT NULL,
-		is_active   Bool NOT NULL
+		is_active        Bool NOT NULL
 	,CONSTRAINT tree_sub_category_PK PRIMARY KEY (id)
 	,CONSTRAINT tree_sub_category_tree_category_FR FOREIGN KEY (id_tree_category) REFERENCES tree_category(id)
 )ENGINE=InnoDB;
@@ -120,7 +124,8 @@ CREATE TABLE customer_order_detail(
         id                Varchar (36) NOT NULL ,
         quantity          Int NOT NULL ,
         id_tree           Varchar (36) NOT NULL ,
-        id_customer_order Varchar (36) NOT NULL
+        id_customer_order Varchar (36) NOT NULL,
+        is_active         Bool NOT NULL
 	,CONSTRAINT customer_order_detail_PK PRIMARY KEY (id)
 
 	,CONSTRAINT customer_order_detail_tree_FK FOREIGN KEY (id_tree) REFERENCES tree(id)
@@ -136,7 +141,8 @@ CREATE TABLE supplier_order_detail(
         id                Varchar (36) NOT NULL ,
         quantity          Int NOT NULL ,
         id_supplier_order Varchar (36) NOT NULL ,
-        id_tree           Varchar (36) NOT NULL
+        id_tree           Varchar (36) NOT NULL,
+        is_active         Bool NOT NULL
 	,CONSTRAINT supplier_order_detail_PK PRIMARY KEY (id)
 
 	,CONSTRAINT supplier_order_detail_supplier_order_FK FOREIGN KEY (id_supplier_order) REFERENCES supplier_order(id)
